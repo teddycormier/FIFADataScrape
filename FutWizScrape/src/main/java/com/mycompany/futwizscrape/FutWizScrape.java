15dff1b2039e17d511523a8a698f7ca9ba8d2e77
teddycormier <teddycormier@10.182.133.173>
2023-03-01 23:31:49 -0500

    full transfer of code

--- FutWizScrape/src/main/java/com/mycompany/futwizscrape/FutWizScrape.java
+++ FutWizScrape/src/main/java/com/mycompany/futwizscrape/FutWizScrape.java
@@ -1,97 +1,95 @@
 package com.mycompany.futwizscrape;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Callable;
-
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class FutWizScrape {
 
-    private static final int THREAD_POOL_SIZE = 5;
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final int MAX_PAGES = 800;
+    private static final AtomicInteger PRINTED_LINE_COUNTER = new AtomicInteger(0);
+    private static final AtomicBoolean KEEP_SEARCHING = new AtomicBoolean(true);
 
-    private static class PlayerSearchTask implements Runnable {
+    private static class PlayerSearchTask implements Callable<Integer> {
 
         private final String playerNameInput;
-        private final int pageNum;
+        private final int startPage;
+        private final int endPage;
         private final Scanner scanner;
-        private int printedLine = 0;
 
-        public PlayerSearchTask(String playerNameInput, int pageNum, Scanner scanner) {
+        public PlayerSearchTask(String playerNameInput, int startPage, int endPage, Scanner scanner) {
             this.playerNameInput = playerNameInput;
-            this.pageNum = pageNum;
+            this.startPage = startPage;
+            this.endPage = endPage;
             this.scanner = scanner;
         }
 
         @Override
-        public void run() {
-            String url = "https://www.futwiz.com/en/fifa23/players?page=" + pageNum;
-            Document doc = null;
+        public Integer call() {
+            for (int pageNum = startPage; pageNum <= endPage; pageNum++) {
+                String url = "https://www.futwiz.com/en/fifa23/players?page=" + pageNum;
+                Document doc;
 
-            try {
-                doc = Jsoup.connect(url).get();
-            } catch (HttpStatusException e) {
-                if (e.getStatusCode() == 500) {
-                    System.out.println("Retrying connection...");
-                    if (pageNum < 800) {
-                        new Thread(new PlayerSearchTask(playerNameInput, pageNum, scanner)).start();
-                        return;
-                    } else {
-                        return;
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("\nError: Failed to connect to server. Please check your internet connection. - Retrying...");
-                new Thread(new PlayerSearchTask(playerNameInput, pageNum, scanner)).start();
-                return;
-            }
-
-            Elements playerRows = doc.select(".table-row");
-            int printedLine = 0;
-            for (Element playerRow : playerRows) {
-                String playerName = playerRow.select(".player a[href*=player] b").text();
-                String playerRating = playerRow.select(".otherversion23-txt").text();
-                String playerPrice = playerRow.select("td:nth-of-type(5)").text();
-                if (playerNameInput.equalsIgnoreCase(playerName)) {
-                    if (printedLine < 3) {
-                        if (playerPrice.equals("")) {
-                            playerPrice = "sbc/untradable";
+                try {
+                    doc = Jsoup.connect(url).get();
+                } catch (HttpStatusException e) {
+                    if (e.getStatusCode() == 500) {
+                        System.out.println("Retrying connection...");
+                        if (pageNum < MAX_PAGES) {
+                            return pageNum;
+                        } else {
+                            return null;
                         }
-                        System.out.println(playerName + "  Overall: " + playerRating
-                                + "  Last Sold For: " + playerPrice);
-                        printedLine++;
-                    } else {
-                        System.out.println("Would you like me to keep searching? (y/n)");
-                        String YNresponse = scanner.nextLine();
-                        if (YNresponse.equalsIgnoreCase("y")) {
-                            if (playerPrice.equals("")) {
+                    }
+                    return null;
+                } catch (IOException e) {
+                    System.out.println("\nError: Failed to connect to server. Please check your internet connection. - Retrying...");
+                    return pageNum;
+                }
+
+                Elements playerRows = doc.select(".table-row");
+                for (Element playerRow : playerRows) {
+                    String playerName = playerRow.select(".player a[href*=player] b").text();
+                    String playerRating = playerRow.select(".otherversion23-txt").text();
+                    String playerPrice = playerRow.select("td:nth-of-type(5)").text();
+                    if (playerNameInput.equalsIgnoreCase(playerName)) {
+                        if (PRINTED_LINE_COUNTER.get() < 3) {
+                            if (playerPrice.isEmpty()) {
                                 playerPrice = "sbc/untradable";
                             }
-                            System.out.println(playerName + "  Overall: " + playerRating
-                                    + "  Last Sold For: " + playerPrice);
-                            printedLine++;
-                        } else {
-                            break;
+                            System.out.println(playerName + "  Overall: " + playerRating + "  Last Sold For: " + playerPrice);
+                            System.out.println(PRINTED_LINE_COUNTER.incrementAndGet());
+                        }
+                        if (PRINTED_LINE_COUNTER.get() == 3) {
+                            System.out.print("\nDo you want to continue? (Y/N): ");
+                            String input = scanner.nextLine();
+                            if (input.equalsIgnoreCase("n")) {
+                                KEEP_SEARCHING.set(false);
+                                return null;
+                            } else {
+                                PRINTED_LINE_COUNTER.set(0);
+                            }
                         }
                     }
                 }
             }
+            return null;
         }
-
     }
 
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Scanner scanner = new Scanner(System.in);
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        int pagesPerThread = MAX_PAGES / THREAD_POOL_SIZE;
 
         while (true) {
             System.out.print("\nENTER PLAYER NAME ('STOP' TO QUIT): \n");
@@ -102,14 +100,21 @@ public class FutWizScrape {
                 return;
             }
 
-            List<Callable<Object>> tasks = new ArrayList<>();
-            for (int pageNum = 0; pageNum < 800; pageNum++) {
-                tasks.add(Executors.callable(new PlayerSearchTask(playerNameInput, 
-                                             pageNum, scanner)));
+            PRINTED_LINE_COUNTER.set(0);
+            KEEP_SEARCHING.set(true);
+            for (int i = 0; i < THREAD_POOL_SIZE && KEEP_SEARCHING.get(); i++) {
+                int startPage = i * pagesPerThread;
+                int endPage = (i + 1) * pagesPerThread;
+                if (i == THREAD_POOL_SIZE - 1) {
+                    endPage = MAX_PAGES;
+                }
+                Future<Integer> future = executor.submit(new PlayerSearchTask(playerNameInput, startPage, endPage, scanner));
+                Integer failedPageNum = future.get();
             }
-            executor.invokeAll(tasks);
-            System.out.println("Player not found.");
+
+            if (KEEP_SEARCHING.get()) {
+                System.out.println("Player not found.");
+            }
         }
     }
-
 }
